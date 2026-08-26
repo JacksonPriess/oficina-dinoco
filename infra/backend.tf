b05e85c0dde94b45d8a5dd3d911cd867b1dcc0d9
Jackson Priess JP <7551889+JacksonPriess@users.noreply.github.com>
2026-08-25 21:53:30 -0300

    Removido arquivos do terraform desse repositório.

--- infra/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "oficina-state-priess951"
-    key    = "infra/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
--- infra/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "postgres_endpoint" {
-  description = "Endereço de conexão do banco de dados"
-  value       = aws_db_instance.postgres.endpoint
-}
-
-output "eks_cluster_name" {
-  description = "Nome do Cluster EKS"
-  value       = aws_eks_cluster.eks_cluster.name
-}
